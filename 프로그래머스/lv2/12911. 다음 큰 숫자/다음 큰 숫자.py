ebe31e284900a474bfe56cb3a069396349d3623d
Lee816 <106318494+Lee816@users.noreply.github.com>
2023-08-24 16:59:29 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/lv2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -4,3 +4,5 @@ def solution(n):
         answer += 1
         if list(str(bin(n))).count('1') == list(str(bin(answer))).count('1'):
             return answer
+        
+    # bin() 값에 .count() 사용가능
